aa7430c59ea4464a6993e8a671abc0f65b9a4699
deepakrathod32 <deepakrathod7132@gmail.com>
2022-06-20 23:01:23 +0530

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import App from './App';
-import Navbar from './Navbar';
-import Loader from './Loader';
-import Comment from './Comment';
-import FriendsList from './FriendList';
-import CreatePost from './CreatePost';
-
-export { App, Navbar, Loader, Comment, FriendsList, CreatePost };
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
-import Settings from './Settings';
-import UserProfile from './UserProfile';
-import Post from './Post';
-
-export { Home, Login, Signup, Settings, UserProfile, Post };
